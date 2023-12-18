b8b8d5b3ef84c86ad942018a66f2d535c2b0a21b
lakhaloumaima <lakhalouma6@gmail.com>
2023-12-18 17:36:34 +0100

    fix downloads linq

--- app/src/main/java/com/example/MyApplication/DownloadActivity.kt
+++ app/src/main/java/com/example/MyApplication/DownloadActivity.kt
@@ -24,42 +24,42 @@ class DownloadActivity : AppCompatActivity() {
             Notes(
                 R.drawable.baseline_sim_card_download_24,
                 "Tp 2",
-                "https://riptutorial.com/Download/kotlin-fr.pdf"
+                "https://drive.google.com/file/d/1BmFje9Q_OS5gTi3xeysrGubCU_lBOseA/view?usp=sharing"
             )
         )
         item.add(
             Notes(
                 R.drawable.baseline_sim_card_download_24,
                 "Tp 3",
-                "https://riptutorial.com/Download/kotlin-fr.pdf"
+                "https://drive.google.com/file/d/1UZrnYCbOw9BbWeY5E3CfgBGiJ-4g-PWp/view?usp=sharing"
             )
         )
         item.add(
             Notes(
                 R.drawable.baseline_sim_card_download_24,
                 "Tp 4",
-                "https://riptutorial.com/Download/kotlin-fr.pdf"
+                "https://drive.google.com/file/d/16McFflgygQ37-GAz80dh5k1fOH6vrvfa/view?usp=sharing"
             )
         )
         item.add(
             Notes(
                 R.drawable.baseline_sim_card_download_24,
                 "Tp 5",
-                "https://riptutorial.com/Download/kotlin-fr.pdf"
+                "https://drive.google.com/file/d/10BSDZfnW9icoNUd_0VbC8r23HhIJQGN5/view?usp=sharing"
             )
         )
         item.add(
             Notes(
                 R.drawable.baseline_sim_card_download_24,
                 "Tp 6",
-                "https://riptutorial.com/Download/kotlin-fr.pdf"
+                "https://drive.google.com/file/d/1ePbCusXTm0Hmx6nRLV3SoJPSz7laRA-5/view?usp=sharing"
             )
         )
         item.add(
             Notes(
                 R.drawable.baseline_sim_card_download_24,
                 "Tp 7",
-                "https://riptutorial.com/Download/kotlin-fr.pdf"
+                "https://drive.google.com/file/d/1C3HdXAL9XfzSJGfdx7HmnPJMmt-KrzjR/view?usp=sharing"
             )
         )
 
